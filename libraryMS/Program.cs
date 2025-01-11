582d64c149e84707b9066b5141ce44a337dd4074
niluminda <kvcniluminda@gmail.com>
2025-01-11 23:21:43 +0530

    second commit

--- libraryMS/Program.cs
+++ libraryMS/Program.cs
@@ -21,10 +21,45 @@ class Program
         BooksStore.Add(new Book(100, "ISBN100", "Fourier Transform", "Butler", "Mathamatics", 10));
 
 
-        Book NewBook = Service.CreateBook();         //adding books from the console
-        BooksStore.Add(NewBook);
+
+
+       ;
+
+        Console.WriteLine("Library Management System");
+
+        Console.WriteLine(
+              "Add new book         -->  add -book\n" +
+              "Add new member       -->  add -mem\n" );
 
         
+
+        string Continuity = "y";
+
+        while (Continuity == "y")
+        {
+            Console.Write("\noperation: ");
+            string Operation_selected = Console.ReadLine();
+
+            switch (Operation_selected)
+            {
+                case "add -book":
+                    Book NewBook = Service.CreateBook();         //adding books from the console
+                    BooksStore.Add(NewBook);
+                    break;
+                case "add -mem":
+                    Member member = Service.CreateMember();
+                    MemberStore.Add(member);
+                    break;
+                default:
+                    Console.WriteLine("you have not give proper input");
+                    break;
+
+            }
+
+            Console.Write("\nmore operations?: y/n  ");
+            Continuity= Console.ReadLine();
+
+        }
 
     }
 }
